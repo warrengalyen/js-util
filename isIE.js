d4dae4c9ae8321ef79895ed39052a2594e4b4bcc
wgalyen <wgalyen@bellhelmets.com>
2019-10-01 08:40:56 -0700

    Initial commit

---
+++ isIE.js
@@ -0,0 +1,4 @@
+module.exports = function() {
+    var ua = navigator.userAgent;
+    return (ua.indexOf("MSIE") > -1 || ua.indexOf("rv:11.0") > -1);
+}
---
+++ splitStr.js
@@ -0,0 +1,20 @@
+module.exports = function(str, className, hasWrap) {
+    var strArr = str.split('');
+    var html = '';
+    if (hasWrap) {
+        for (var i = 0; i < strArr.length; i++) {
+            if (i === 0) html += '<span class="' + className + '-wrap">';
+            html += '<span class="' + className + ' ' + className + '--' + (i + 1) + '">';
+            html += (strArr[i] === ' ') ? '&nbsp' : strArr[i];
+            html += '</span>';
+            if (strArr[i] === ' ') html += '</span><span class="' + className + '-wrap">';
+            if (i === strArr.length - 1) html += '</span>';
+        }
+    } else {
+        for (var i = 0; i < strArr.length; i++) {
+            html += '<span class="' + className + ' ' + className + '--' + (i + 1) + '">';
+            html += (strArr[i] === ' ') ? '&nbsp' : strArr[i];
+            html += '</span>';
+        }
+    }
+}
